763f35abe30089ebdfa6919733e963494d75bcd7
CarlosAMolina <carlosantmolina@gmail.com>
2016-05-14 21:08:22 +0200

    Necessary ignore robots to work with Google

--- editInputHidden.py
+++ editInputHidden.py
@@ -3,6 +3,7 @@
 import mechanize
 
 br = mechanize.Browser()
+br.set_handle_robots(False) # ignore robots. Avoid mechanize._response.httperror_seek_wrapper: HTTP Error 403: request disallowed by robots.txt
 br.open("http://www.google.com")
 
 for form in br.forms():
